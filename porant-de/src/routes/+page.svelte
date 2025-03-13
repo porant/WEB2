d54b7be8253abcee0b79e840ca17d154fa6c2e2e
porant <porant@gmail.com>
2025-03-13 18:51:31 +0100

    modified:   porant-de/src/routes/+page.svelte 	deleted:    porant-de/src/routes/Logo.svelte

--- porant-de/src/routes/+page.svelte
+++ porant-de/src/routes/+page.svelte
@@ -3,10 +3,31 @@
   <link rel="icon" type="image/x-icon" href="/favicon.ico" />
 </svelte:head>
 
+
 <script lang="ts">
-	import Logo from "./Logo.svelte";
-	import MyAnims from "./MyAnims.svelte";
+  import GlitchText from "./GlitchText.svelte";
+  import MyAnims from "./MyAnims.svelte";
+  import { AppBar } from '@skeletonlabs/skeleton-svelte';
+  import { Send, Mail, BotMessageSquare } from '@lucide/svelte';
 </script>
 
-<Logo />
+<AppBar>
+  {#snippet lead()}
+  <span class="green pt-1">
+    <GlitchText text="PORANT.de"/>
+  </span>
+  {/snippet}
+  {#snippet trail()}
+    <a href="mailto:porant@gmail.com" class="btn green"><Mail /></a>
+    <a href="https://discord.gg/e6wkfyBDGV" class="btn green"><BotMessageSquare/></a>
+    <a href="https://t.me/P0RANT" class="btn green"><Send/></a>
+  {/snippet}
+</AppBar>
+
 <MyAnims />
+
+<style>
+    .green{
+        color: #d4fa0f;
+    }
+</style>
