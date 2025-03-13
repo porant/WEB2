f132d105baff5e948f076443de197e2a11f2e628
porant <porant@gmail.com>
2025-03-13 14:30:30 +0100

    modified:   porant-de/src/routes/+layout.svelte 	modified:   porant-de/src/routes/Logo.svelte 	modified:   porant-de/svelte.config.js

--- porant-de/src/routes/Logo.svelte
+++ porant-de/src/routes/Logo.svelte
@@ -1,19 +1,17 @@
 
 <script lang="ts">
-	import GlitchText from "./GlitchText.svelte";
     import { Send, Mail, BotMessageSquare } from '@lucide/svelte';
 
 
 </script>
 
-<div class="card card-hover p-4 text-center allign-center">
-    <GlitchText text = "PORANT.DE"/>
-    <div class="flex justify-center">
+
+    <div class="flex justify-center pt-6 gap-6">
         <a href="mailto:porant@gmail.com"><Mail /></a>
         <a href="https://discord.gg/e6wkfyBDGV"><BotMessageSquare/></a>
         <a href="https://t.me/P0RANT"><Send/></a>
     </div>
-</div>
+
 
 
 
--- porant-de/svelte.config.js
+++ porant-de/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -15,4 +15,15 @@ const config = {
 	}
 };
 
-export default config;
+export default {
+	kit: {
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
