28e3dfb3e1ccfaee183bd434ed1dc1f3dc025765
porant <porant@gmail.com>
2025-04-05 14:25:08 +0200

    modified:   porant-de/package-lock.json 	modified:   porant-de/package.json 	modified:   porant-de/src/routes/+page.svelte 	new file:   porant-de/src/routes/Dashboard.svelte

--- porant-de/src/routes/+page.svelte
+++ porant-de/src/routes/+page.svelte
@@ -7,6 +7,7 @@
 <script lang="ts">
   import GlitchText from "./GlitchText.svelte";
   import MyAnims from "./MyAnims.svelte";
+  import Dashboard from "./Dashboard.svelte";
   import { AppBar } from '@skeletonlabs/skeleton-svelte';
   import { Send, Mail, BotMessageSquare } from '@lucide/svelte';
 </script>
@@ -23,11 +24,31 @@
     <a href="https://t.me/P0RANT" class="btn green" target="_blank"><Send/></a>
   {/snippet}
 </AppBar>
-
-<MyAnims />
+<div class="flex items-center justify-center h-screen">
+  <Dashboard />
+</div>
 
 <style>
     .green{
         color: #d4fa0f;
     }
+
+    :global(body) {
+      background:
+		  radial-gradient(50% 50% at 100% 0,#101010 0%  5% ,#171717 6%  15%,#101010 16% 25%,#171717 26% 35%,#101010 36% 45%,
+		   #171717 46% 55%,#101010 56% 65%,#171717 66% 75%,#101010 76% 85%,#171717 86% 95%,
+		   #0000 96%),
+		  radial-gradient(50% 50% at 0 100%,#101010 0%  5% ,#171717 6%  15%,#101010 16% 25%,#171717 26% 35%,#101010 36% 45%,
+		   #171717 46% 55%,#101010 56% 65%,#171717 66% 75%,#101010 76% 85%,#171717 86% 95%,
+		   #0000 96%),
+		  radial-gradient(50% 50%,#101010 0%  5% ,#171717 6%  15%,#101010 16% 25%,#171717 26% 35%,#101010 36% 45%,
+		   #171717 46% 55%,#101010 56% 65%,#171717 66% 75%,#101010 76% 85%,#171717 86% 95%,
+		   #0000 96%),
+		  radial-gradient(50% 50%,#101010 0%  5% ,#171717 6%  15%,#101010 16% 25%,#171717 26% 35%,#101010 36% 45%,
+		   #171717 46% 55%,#101010 56% 65%,#171717 66% 75%,#101010 76% 85%,#171717 86% 95%,
+		   #0000 96%) 30px 30px;
+	    background-size: 60px 60px;
+	    background-color: #101010;
+    }
+
 </style>
---
+++ porant-de/src/routes/Dashboard.svelte
@@ -0,0 +1,17 @@
+
+<div class="card w-full max-w-md preset-glass-neutral p-4 text-center">
+    <form class="mx-auto w-full max-w-md space-y-4">
+        <input type="text" class="input" placeholder="Username" />
+        <input type="password" class="input" placeholder="Password" />
+        <button class="btn">Login</button>
+    </form>
+</div>
+
+<style>
+
+.preset-glass-neutral {
+    background: color-mix(in oklab, var(--color-surface-50-950) 30%, transparent);
+    box-shadow: 0 0px 30px color-mix(in oklab, var(--color-surface-50-950) 30%, transparent) inset;
+    backdrop-filter: blur(16px);
+  }
+</style>
